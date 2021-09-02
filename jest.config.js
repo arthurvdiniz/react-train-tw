ebd9e7be544ca887b03242b2a7034aa8cb0901bf
Cristóvão Olegário <cristovaoolegario@gmail.com>
2021-09-02 15:22:35 -0300

    Adding jest config and unit test example.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.{ts,tsx}',
+      '!**/*.d.ts'
+    ],
+    coverageDirectory: 'coverage',
+    setupFilesAfterEnv: ['<rootDir>/src/config/jest-setup.ts'],
+    testPathIgnorePatterns: [
+      '<rootDir>/node_modules/'
+    ],
+    testEnvironment: 'jsdom',
+    transform: {
+      '.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    moduleNameMapper: {
+      '@/(.*)': '<rootDir>/src/$1'
+    }
+  }
---
+++ src/sum.test.ts
@@ -0,0 +1,5 @@
+import {sum} from "./sum"
+
+test("Test if 1 + 1 is equal 2", () => {
+    expect(sum(1,1)).toBe(2)
+})
