2570576837e7f9ef347f11f400bd46c695edcbd2
marianaram0s <mariana.r1@thoughtworks.com>
2021-09-06 15:26:46 -0300

    using screen to access queries

--- src/components/Terms/Terms.test.tsx
+++ src/components/Terms/Terms.test.tsx
@@ -1,40 +1,40 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Terms from './Terms'
 
 test('should renders a message and checkbox', () => {
-  const { container, getByText, getByTestId } = render(<Terms />)
-  expect(getByText('Terms of Service')).toBeInTheDocument()
-  expect(getByText('Accept terms')).toBeInTheDocument()
-  expect(getByTestId('terms-of-service-text')).toBeInTheDocument()
+  render(<Terms />)
+  expect(screen.getByText('Terms of Service')).toBeInTheDocument()
+  expect(screen.getByText('Accept terms')).toBeInTheDocument()
+  expect(screen.getByTestId('terms-of-service-text')).toBeInTheDocument()
 })
 
 test('should keep text color black when checkbox is not checked', () => {
-  const { container, getByText, getByTestId } = render(<Terms />)
-  var checkbox = getByText('Accept terms')
+  render(<Terms />)
+  var checkbox = screen.getByText('Accept terms')
 
-  expect(getByTestId('terms-of-service-text').style.color).toBe('black')
+  expect(screen.getByTestId('terms-of-service-text').style.color).toBe('black')
   expect(checkbox).not.toBeChecked()
 })
 
 test('should change text color when checkbox is checked', () => {
-  const { container, getByTestId } = render(<Terms />)
-  var checkbox = getByTestId('terms-of-service-checkbox')
+  render(<Terms />)
+  var checkbox = screen.getByTestId('terms-of-service-checkbox')
 
   fireEvent.click(checkbox)
 
-  expect(getByTestId('terms-of-service-text').style.color).toBe('green')
+  expect(screen.getByTestId('terms-of-service-text').style.color).toBe('green')
 })
 
 test('should update text color when checkbox is checked and not checked', () => {
-  const { container, getByTestId } = render(<Terms />)
-  var checkbox = getByTestId('terms-of-service-checkbox')
+  render(<Terms />)
+  var checkbox = screen.getByTestId('terms-of-service-checkbox')
 
-  expect(getByTestId('terms-of-service-text').style.color).toBe('black')
-
-  fireEvent.click(checkbox)
-  expect(getByTestId('terms-of-service-text').style.color).toBe('green')
+  expect(screen.getByTestId('terms-of-service-text').style.color).toBe('black')
 
   fireEvent.click(checkbox)
-  expect(getByTestId('terms-of-service-text').style.color).toBe('black')
+  expect(screen.getByTestId('terms-of-service-text').style.color).toBe('green')
+
+  fireEvent.click(checkbox)
+  expect(screen.getByTestId('terms-of-service-text').style.color).toBe('black')
 })
